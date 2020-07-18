7f388506f8f9983355a04e56d7a6b4bf10d9affd
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-18 15:19:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @total += price * quantity
     quantity.times {@item_list << title}
-    @last_item_price = price
+    @last_item_price = price * quantity
   end
 
   def apply_discount
