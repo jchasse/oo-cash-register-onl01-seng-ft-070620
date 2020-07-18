1fee078c4669c79716a606d4ad1cb2eb2fb22d52
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-18 14:42:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,5 +17,6 @@ class CashRegister
     # binding.pry
     discount_amount = @total * @discount/100
     @total -= discount_amount
+    puts "After the discount, the total comes to #{total}."
   end
 end
